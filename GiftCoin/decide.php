6dc2f0d99ef51e7aeaa580f7d42e39cb7a738337
jimmytidey <jimmytidey@gmail.com>
2012-10-01 21:07:19 +0100

    automated commit

---
+++ GiftCoin/decide.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset=utf-8 />
+	<title>Redeemer</title>
+	<meta name="description" content="Redeemer" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+
+</head> 
+
+<body id='decide'>
+    <div id='container'>
+
+    </div>
+</body>
+</html>
